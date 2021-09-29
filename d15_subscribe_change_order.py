86e3c3ba96e6edf23bff23d82d9d22e23977d433
eyelash500 <eyelash.94500@gmail.com>
2021-09-29 23:35:06 +0800

    Finished: day15

---
+++ d15_subscribe_change_order.py
@@ -0,0 +1,80 @@
+import threading
+import time
+from datetime import datetime
+
+import shioaji as sj
+
+
+class trader:
+    """The Shioaji Object"""
+
+    def __init__(self) -> None:
+        self.simulation = True  # 是否為測試環境
+        self.id = "PAPIUSER07"
+        self.pwd = "2222"
+        self.api = None
+
+    def login(self, id=None, pwd=None, simulation=True):
+        """Login to Shioaji.
+
+        Args:
+            id(str): user ID
+            pwd(str): the login password
+
+        Returns:
+            bool: True is login successfully, False is not.
+        """
+
+        print(f"=start login-{datetime.now().strftime('%Y%m%d')}")
+        if id and pwd:
+            self.id = id
+            self.pwd = pwd
+
+        try:
+            # 登入 shioaji
+            self.api = sj.Shioaji(simulation=simulation)
+            self.api.login(person_id=self.id, passwd=self.pwd)
+        except Exception as exc:
+            print(f"id={self.id}, pwd={self.pwd}...{exc}")
+            return False
+
+        return True
+
+    def _get_subscribe(self) -> bool:
+        """Get the subscibe format."""
+
+        print(self.api.quote.subscribe)
+
+        return True
+
+    def subscribe(self, contract):
+        self.api.quote.subscribe(contract, quote_type=sj.constant.QuoteType.Tick)
+
+    def unsubscribe(self, contract):
+        self.api.quote.unsubscribe(contract, quote_type=sj.constant.QuoteType.Tick)
+
+
+def scraper():
+    time.sleep(10)
+
+
+timer = threading.Thread(target=scraper)  # 建立執行緒
+
+t = trader()
+t.login()
+t._get_subscribe()
+t.subscribe(t.api.Contracts.Futures.TXF["TXF202110"])
+timer.start()  # 執行
+t.quote.unsubscribe(t.Contracts.Futures.TXF["TXF202110"])
+
+
+@t.api.quote.on_quote
+def quote_callback(topic: str, quote: dict):
+    """Print quote info.
+
+    已經沒有 from shioaji import TickSTKv1了，
+    所以不能用範例QuoteVersion.v1，
+    只能用QuoteVersion.v0
+    """
+
+    print(f"Topic: {topic}, Quote: {quote}")
